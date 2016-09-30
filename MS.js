3064dc637bb70db89e4ec9fcbc17dd791e4682e1
Michael Scott <michaelascott55@aol.com>
2016-09-29 20:34:38 -0400

    Initial Commit

---
+++ MS.js
@@ -0,0 +1,21 @@
+
+function loadPage(page)
+{
+	$( "#pageTitle" ).add(".mainBody").add(".bodyText").fadeIn("slow");
+}
+
+function pageChange(nextPage)
+{
+  	$( "#pageTitle" ).add(".mainBody").add(".bodyText").fadeOut( "slow", newPage(nextPage) );
+}
+
+
+function newPage(nextPage)
+{
+	if(nextPage == "index") document.location.href = "index.html";
+	if(nextPage == "contact") document.location.href = "contact.html";
+	if(nextPage == "experience") document.location.href = "EE.html";
+	if(nextPage == "skills") document.location.href = "skills.html";
+	if(nextPage == "achievements") document.location.href = "achievements.html";
+	if(nextPage == "projects") document.location.href = "projects.html";
+}
