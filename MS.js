7c21233e94153845de77bbe4004ad3c5710557f6
Michael Allen Scott <michaelallenscott55@gmail.com>
2017-06-25 13:01:49 -0400

    Changed page to route to projects page until work has begun

--- MS.js
+++ MS.js
@@ -1,22 +1,3 @@
-
-function loadPage(page)
-{
-	$( "#pageTitle" ).add(".mainBody").add(".bodyText").add("#copyright").add("#copyrightText").fadeIn("slow");
-}
-
-function pageChange(nextPage)
-{
-  	$( "#pageTitle" ).add(".mainBody").add(".bodyText").add("#copyright").add("#copyrightText").fadeOut( "slow", newPage(nextPage) );
-	
-}
-
-
-function newPage(nextPage)
-{
-	if(nextPage == "index") document.location.href = "index.html";
-	if(nextPage == "contact") document.location.href = "contact.html";
-	if(nextPage == "experience") document.location.href = "EE.html";
-	if(nextPage == "skills") document.location.href = "skills.html";
-	if(nextPage == "achievements") document.location.href = "achievements.html";
-	if(nextPage == "projects") document.location.href = "projects.html";
+function loadPage() {
+	window.location.replace('https://michaelallenscott.github.io/Projects/');
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # MichaelAllenScott.github.io
-Personal website - in progress
+Personal website (in progress). Currently just routes to the [project's page](https://michaelallenscott.github.io/Projects/). 
