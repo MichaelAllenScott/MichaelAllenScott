1e9a03aecdd26b3f360fa014478f2bf7eaee214a
Michael Scott <michaelallenscott55@gmail.com>
2020-07-27 20:03:50 -0400

    Optimized Site

--- js/common.js
+++ js/common.js
@@ -2,10 +2,10 @@ function openResumePDF() {
     window.open('./assets/Michael-Scott-Resume.pdf');
 }
 
-$(window).bind("load", function() {
+window.onload = () => {
     setTimeout(function() 
     {
-        $(".main-body").show();
-        $(".loading").hide();
+        document.getElementById("main-body").style.display = "block";
+        document.getElementById("loading").style.display = "none";
     }, 1000);
-});
+}
